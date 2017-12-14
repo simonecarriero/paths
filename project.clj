9968813f9d700137a01aa3a8db076b3aaa30d263
Simone Carriero <simonecarriero@gmail.com>
2017-12-14 23:27:36 +0100

    Move to clj subfolder

--- project.clj
+++ project.clj
@@ -1,3 +1,6 @@
 (defproject paths "1.0.0"
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :source-paths ["src/clj"]
 
+  :test-paths ["test/clj"]
+
+  :dependencies [[org.clojure/clojure "1.8.0"]])
